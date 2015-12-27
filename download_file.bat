56d171caae25666e39e16c7be5eb77975ebabd79
Ben Haos <koreahaos@gmail.com>
2015-12-27 12:58:29 +0900

    Works but looks sloppy

--- download_file.bat
+++ download_file.bat
@@ -1,3 +1,9 @@
-echo "Hello bat World!"
+echo "Hello Bat World!"
 echo "This is a test!"
-cmd /K
+bitsadmin /transfer myDownloadJob /download /priority normal http://www.rividium.com/images/testing1.jpg %~dp0downloaded_file.jpg
+rem client = new-object System.Net.WebClient
+
+rem client.DownloadFile("http://www.rividium.com/images/testing1.jpg", "%~dp0downloaded_file.jpg")
+REM echo %~dp0downloaded_file.jpg
+pause
+REM cmd /K
